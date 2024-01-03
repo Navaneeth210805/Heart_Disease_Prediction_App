e3c2e440c1c94fba18d5df315f6dae96b88e3ddf
Navaneeth210805 <134401919+Navaneeth210805@users.noreply.github.com>
2024-01-03 14:17:58 +0530

    Update heart_problem_prediciton_app.py

--- heart_problem_prediciton_app.py
+++ heart_problem_prediciton_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import joblib
-
 with open(r'C:\Users\Navaneeth\Desktop\Python\model1.pkl', 'rb') as file:
     loaded_content = pickle.load(file)
     st.write(loaded_content)
